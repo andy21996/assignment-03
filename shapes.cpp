92699110c11772a64c35731a3edc0505965d453c
andy21996 <andy21996@gmail.com>
2015-10-06 19:21:17 -0700

    shapes.cpp

--- shapes.cpp
+++ shapes.cpp
@@ -1,37 +1,29 @@
 /* ----------------------------------------------------------------------------
- * Copyright &copy; 2015 Ben Blazak <bblazak@fullerton.edu>
- * Released under the [MIT License] (http://opensource.org/licenses/MIT)
- * ------------------------------------------------------------------------- */
-
+* Copyright &copy; 2015 Duy Nguyen <andy21996@fullerton.edu>
+* Released under the [MIT License] (http://opensource.org/licenses/MIT)
+* ------------------------------------------------------------------------- */
 #include <cmath>
 using std::abs;
 
-#include "shapes.h"
+#include "Shape.h"
 
-// ----------------------------------------------------------------------------
-
-Rectangle::Rectangle(Point center, float dx, float dy) : center(center),
-                                                         dx(dx),
-                                                         dy(dy) {}
-
-bool Rectangle::contains(const Point & p) const {
-    return    abs( p.getX() - center.getX() ) <= dx
-           && abs( p.getY() - center.getY() ) <= dy;
+Rectangle::Rectangle(Point corner, double length, double width) : corner(corner), length(length), width(width) {}
+bool Rectangle::contains(const Point & p) const
+{
+	return abs(p.GetX() - corner.GetX()) <= length && abs(p.GetY() - corner.GetY()) <= width;
 }
 
-Square::Square(Point center, float side)
-    : Rectangle(center, side/2, side/2) {}
+Square::Square(Point corner, double side)
+{
 
-// ----------------------------------------------------------------------------
-
-Ellipse::Ellipse(Point f1, Point f2, float radius) : f1(f1),
-                                                     f2(f2),
-                                                     radius(radius) {}
-
-bool Ellipse::contains(const Point & p) const {
-    return p.distance(f1) + p.distance(f2) <= radius;
 }
 
-Circle::Circle(Point center, float radius)
-    : Ellipse(center, center, radius) {}
+Ellipse::Ellipse(Point foci1, Point foci2, double distance) : foci1(foci1), foci2(foci2), distance(distance) {}
+bool Ellipse::contains(const Point& p) const
+{
+	return p.distance(foci1) + p.distance(foci2) <= distance;
+}
 
+Circle::Circle(Point center, double radius) : Ellipse(center, center, radius) 
+{
+}
