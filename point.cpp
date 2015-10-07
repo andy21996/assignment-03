c56902e53ebbd25dc6da8acd1cedc21da4a1c5a9
andy21996 <andy21996@gmail.com>
2015-10-06 19:19:24 -0700

    point.cpp

--- point.cpp
+++ point.cpp
@@ -1,22 +1,33 @@
 /* ----------------------------------------------------------------------------
- * Copyright &copy; 2015 Ben Blazak <bblazak@fullerton.edu>
- * Released under the [MIT License] (http://opensource.org/licenses/MIT)
- * ------------------------------------------------------------------------- */
-
+* Copyright &copy; 2015 Duy Nguyen <andy21996@fullerton.edu>
+* Released under the [MIT License] (http://opensource.org/licenses/MIT)
+* ------------------------------------------------------------------------- */
 #include <cmath>
 using std::pow;
 using std::sqrt;
+#include "Point.h"
 
-#include "point.h"
 
-// ----------------------------------------------------------------------------
-
-Point::Point(float x, float y) : x(x), y(y) {}
-
-float Point::getX() const { return x; }
-float Point::getY() const { return y; }
-
-float Point::distance(const Point & a) const {
-    return sqrt( pow( a.x - this->x, 2 ) + pow( a.y - this->y, 2 ) );
+Point::Point()
+{
+	xPos = 0.0;
+	yPos = 0.0;
+}
+Point::Point(double x, double y)
+{
+	xPos = x;
+	yPos = y;
+}
+double Point::GetX() const
+{
+	return xPos;
+}
+double Point::GetY() const
+{
+	return yPos;
+}
+double Point::distance(const Point & a) const
+{
+	return sqrt(pow(a.xPos - this->xPos, 2) + pow(a.yPos - this->yPos, 2));
 }
 
